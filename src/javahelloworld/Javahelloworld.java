eb11eded52983fedf8fbf5b30f27aea85691b4bd
Isuru <isuru@alliontechnologies.com>
2016-04-06 11:37:50 +0530

    javahelloworld 4

--- src/javahelloworld/Javahelloworld.java
+++ src/javahelloworld/Javahelloworld.java
@@ -16,7 +16,7 @@ public class Javahelloworld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
     }
     
 }
